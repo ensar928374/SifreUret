5c7b6a0e1a405a815c5071ae04844c501f23eda3
kerimcalik <kcalik@gmail.com>
2021-09-12 09:23:08 +0300

    Proje dosyası ekle.

---
+++ SifreUret/PasswordGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SifreUret
+{
+    internal class PasswordGenerator
+    {
+        private const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string digits = "0123456789";
+        private const string specChar = "!#$%&*+-/;<=?@_|~.";
+
+        private static string[] Shuffle(string[] wordArray)
+        {
+            Random random = new Random();
+            for (int i = wordArray.Length - 1; i > 0; i--)
+            {
+                int swapIndex = random.Next(i + 1);
+                string temp = wordArray[i];
+                wordArray[i] = wordArray[swapIndex];
+                wordArray[swapIndex] = temp;
+            }
+            return wordArray;
+        }
+
+        public static string GeneratePassword(int length, bool isSpecialCharacter = true, bool isCaseSensitive = true)
+        {
+            if (length < 8) length = 8;
+
+            ArrayList passwordArray = new ArrayList();
+
+            int numberOfCharacters = (int)Math.Round(length * (isSpecialCharacter ? 0.5 : 0.7));
+            int numberOfDigits = (int)Math.Round(length * 0.3);
+            int numberOfSpecialCharters = (int)Math.Round(length * 0.2);
+
+            for (int j = 0; j < numberOfCharacters; j++)
+            {
+                string randomLetter = letters.ToCharArray()[new Random().Next(0, letters.Length - 1)].ToString();
+                passwordArray.Add(isCaseSensitive ? (new Random().Next() % 2 == 0 ? randomLetter.ToLower() : randomLetter.ToUpper()) : randomLetter);
+
+            }
+            for (var k = 0; k < numberOfDigits; k++)
+            {
+                passwordArray.Add(digits.ToCharArray()[new Random().Next(0, digits.Length - 1)].ToString());
+            }
+
+            if (isSpecialCharacter)
+            {
+                for (int z = 0; z < numberOfSpecialCharters; z++)
+                {
+                    passwordArray.Add(specChar.ToCharArray()[new Random().Next(0, specChar.Length - 1)].ToString());
+                }
+            }
+
+
+            return string.Join("", Shuffle((string[])passwordArray.ToArray(typeof(string))));
+        }
+    }
+}
---
+++ SifreUret/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SifreUret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Üretilecek şifrenin uzunluğunu giriniz: ");
+            int length = int.Parse(Console.ReadLine());
+            
+            Console.Write("Özel karakter olsun mu (e/h)?: ");
+            bool isSpecialChars = Console.ReadLine() == "e";
+
+            Console.Write("Büyük/küçük harf olsun mu (e/h)?: ");
+            bool isCaseSensitive = Console.ReadLine() == "e";
+
+            Console.Write($"\n\nÜretilen şifre: {PasswordGenerator.GeneratePassword(length, isSpecialChars, isCaseSensitive)}\n");
+
+            Console.Read();
+
+        }
+    }
+}
